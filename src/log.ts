4b214122ada9af4650cc29db3b7f587b35b56950
zhoufeng <15125505@qq.com>
2023-07-07 14:07:31 +0800

    修改：color文件去掉了（被合并到了log中）。 修改：增加了打包的一些内容。

--- src/log.ts
+++ src/log.ts
@@ -2,27 +2,33 @@
 // Description: 日志模块
 // Date: 2023-07-07 09:52
 
-import {Color} from "./color";
 
-export class z {
+export class Log {
     static debug(...args: any[]) {
         console.log(...args);
     }
 
     static log(...args: any[]) {
-        console.log(Color.fg(args.join(' '), 32));
+        console.log(this.color(args.join(' '), 32));
     }
 
     static info(...args: any[]) {
-        console.log(Color.fg(args.join(' '), 36));
+        console.log(this.color(args.join(' '), 36));
     }
 
     static warn(...args: any[]) {
-        console.log(Color.fg(args.join(' '), 33));
+        console.log(this.color(args.join(' '), 33));
     }
 
     static error(...args: any[]) {
-        console.log(Color.fg(args.join(' '), 31));
+        console.log(this.color(args.join(' '), 31));
+    }
+
+    private static color(text: string, crText: number): string {
+        if (crText < 30 || crText > 37) {
+            return text;
+        }
+        return '\x1b[0;' + crText + 'm' + text + '\x1b[0m';
     }
 
 }
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/log.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    target: 'node',
+    mode: 'production',
+    devtool: 'source-map',
+};
